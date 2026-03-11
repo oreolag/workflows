91a413ce9299638b8e0cb929c5b4b8e365b625d0
Javier Moya <jmoya@hey.com>
2026-03-11 19:43:42 +0100

    First commit

--- nccl/cmd_spec.sh
+++ nccl/cmd_spec.sh
@@ -1,5 +1,35 @@
 #!/usr/bin/env bash
 
+# new
+NEW_NCCL_DESCRIPTION="Creates a NVIDIA Collective Communications Library (NCCL) project"
+NEW_NCCL_FLAGS=(
+  "name,n,NCCL project name,-,-"
+)
+NEW_NCCL_FLAGS_MANDATORY="name"
+
+# build
+BUILD_NCCL_DESCRIPTION="Builds your NVIDIA Collective Communications Library (NCCL) project"
+BUILD_NCCL_FLAGS=(
+  "name,n,NCCL project name,-,-"
+)
+BUILD_NCCL_FLAGS_MANDATORY="name"
+
+# program
+PROGRAM_NCCL_DESCRIPTION="Programs your NCCL project to a specified device"
+PROGRAM_NCCL_FLAGS=(
+  "name,n,NCCL project name,-,-"
+  "device,d,Device index,-,-"
+)
+PROGRAM_NCCL_FLAGS_MANDATORY="name,device"
+
+# run
+RUN_NCCL_DESCRIPTION="Runs your NCCL project"
+RUN_NCCL_FLAGS=(
+  "name,n,NCCL project name,-,-"
+)
+RUN_NCCL_FLAGS_MANDATORY="name"
+
+# validate
 VALIDATE_NCCL_DESCRIPTION="NVIDIA Collective Communications Library (NCCL) validation"
 VALIDATE_NCCL_FLAGS=(
   "ngpus,g,Number of GPUs,1-8,1"
---
+++ nccl/program.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# example: odev new nccl
+
+# get script location
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SUBCOMMAND="$(basename "${BASH_SOURCE[0]}" .sh)"
+
+# derive from SCRIPT_DIR
+CLI_NAME="$(basename "$(dirname "$(dirname "$SCRIPT_DIR")")")"
+COMMAND="$(basename "$SCRIPT_DIR")"
+ODEV_PATH="${ODEV_PATH:-"$(dirname "$SCRIPT_DIR")"}"
+
+# get hostname
+url="${HOSTNAME}"
+hostname="${url%%.*}"
+
+# format
+bold=$(tput bold)
+italic=$(tput sitm 2>/dev/null || true)
+normal=$(tput sgr0)
+
+# constants
+# ...
+
+# check on users
+# ...
+
+# check on tools
+# ...
+
+# set KEY
+KEY="$(printf '%s_%s' "$COMMAND" "$SUBCOMMAND" | tr '[:lower:]' '[:upper:]')"
+
+# read command description, command flags, and mandatory flags
+command_description="$("$ODEV_PATH/src/cmd_description_read.sh" "$ODEV_PATH" "$KEY")"
+mapfile -t flags < <("$ODEV_PATH/src/cmd_flags_read.sh" "$ODEV_PATH" "$KEY")
+mandatory_flags="$("$ODEV_PATH/src/cmd_mandatory_flags_read.sh" "$ODEV_PATH" "$KEY")"
+
+# (maybe) print help
+print_range="1"
+print_default="0"
+print_both="0"
+"$ODEV_PATH/src/help_print.sh" --maybe \
+  "$CLI_NAME" "$COMMAND" "$SUBCOMMAND" "$command_description" \
+  "$print_range" "$print_default" "$print_both" \
+  "${flags[@]}" -- "$@" && exit 0 || true
+
+echo "This is program nccl!"
+
+# author: https://github.com/jmoya82
