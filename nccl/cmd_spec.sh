d744f969d0cfc0e99b3f55695e669f83f73d8d40
Javier Moya <jmoya@hey.com>
2026-03-23 13:41:18 +0100

    Fix

--- nccl/cmd_spec.sh
+++ nccl/cmd_spec.sh
@@ -1,45 +1,45 @@
 #!/usr/bin/env bash
 
 # new
-NEW_DESCRIPTION="Creates a NVIDIA Collective Communications Library (NCCL) project"
+NEW_NCCL_DESCRIPTION="Creates a NVIDIA Collective Communications Library (NCCL) project"
 NEW_FLAGS=(
   "name,n,Project name,-,-"
   "push,p,Push to GitHub,0|1,1"
 )
-NEW_FLAGS_MANDATORY="name,push"
+NEW_NCCL_FLAGS_MANDATORY="name,push"
 
 # build
-BUILD_DESCRIPTION="Builds your nccl project"
-BUILD_FLAGS=(
+BUILD_NCCL_DESCRIPTION="Builds your nccl project"
+BUILD_NCCL_FLAGS=(
   "name,n,Project name,-,-"
 )
-BUILD_FLAGS_MANDATORY="name"
+BUILD_NCCL_FLAGS_MANDATORY="name"
 
 # delete
-DELETE_DESCRIPTION="Deletes a nccl project"
-DELETE_FLAGS=(
+DELETE_NCCL_DESCRIPTION="Deletes a nccl project"
+DELETE_NCCL_FLAGS=(
   "name,n,Project name,-,-"
 )
-DELETE_FLAGS_MANDATORY="name"
+DELETE_NCCL_FLAGS_MANDATORY="name"
 
 # program
-PROGRAM_DESCRIPTION="Programs your nccl project to a specified device"
-PROGRAM_FLAGS=(
+PROGRAM_NCCL_DESCRIPTION="Programs your nccl project to a specified device"
+PROGRAM_NCCL_FLAGS=(
   "name,n,Project name,-,-"
   "device,d,Device index,-,-"
 )
-PROGRAM_FLAGS_MANDATORY="name,device"
+PROGRAM_NCCL_FLAGS_MANDATORY="name,device"
 
 # run
-RUN_DESCRIPTION="Runs your nccl project"
-RUN_FLAGS=(
+RUN_NCCL_DESCRIPTION="Runs your nccl project"
+RUN_NCCL_FLAGS=(
   "name,n,Project name,-,-"
 )
-RUN_FLAGS_MANDATORY="name"
+RUN_NCCL_FLAGS_MANDATORY="name"
 
 # validate
-VALIDATE_DESCRIPTION="NVIDIA Collective Communications Library (NCCL) validation"
-VALIDATE_FLAGS=(
+VALIDATE_NCCL_DESCRIPTION="NVIDIA Collective Communications Library (NCCL) validation"
+VALIDATE_NCCL_FLAGS=(
   "ngpus,g,Number of GPUs,1-8,1"
   "nthreads,t,Threads per process,1-64,1"
   "minbytes,b,Minimum message size,1B|4K|8M|1G,8M"
@@ -48,4 +48,4 @@ VALIDATE_FLAGS=(
   "datatype,d,Specify which datatype to use,int8|half|bfloat16|float,float"
   "stepfactor,f,Multiplication factor between sizes,2|4|8,2"
 )
-VALIDATE_FLAGS_MANDATORY="ngpus,minbytes,maxbytes"
+VALIDATE_NCCL_FLAGS_MANDATORY="ngpus,minbytes,maxbytes"
